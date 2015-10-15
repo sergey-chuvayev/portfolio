67721ca83e993dcebb5540b3c00adde14fec7478
Sergey <sergeypanic@gmail.com>
2015-10-15 13:05:33 +0300

    wait until iframe ready

--- test_page/script.js
+++ test_page/script.js
@@ -1,4 +1,13 @@
+var config = {
+	iframeURL: 'http://localhost:4000/'
+}
+
 $(function() {
+
+	// set iframe url to iframe
+	$('iframe').attr('src', config.iframeURL);
+
+	// pass video and bg links to iframe
 	init({
 		video: 'http://lamcdn.net/specials.lookatme.ru/0000000001/banners/system/clients/top/guerlain/video2.mp4',
 		bg: 'http://www.emoticonswallpapers.com/background/skulls/skulls-background-pattern-0815.gif'
@@ -6,8 +15,11 @@ $(function() {
 });
 
 function init(params) {
-	// setTimeout(function() {
-		var win = document.getElementsByTagName("iframe")[0].contentWindow;
-		win.postMessage(params, 'http://localhost:4000/');
-	// }, 100);
+	var iframe = $('iframe').get(0);
+	
+	// wait untill iframe loaded
+	iframe.onload = function() {
+		var win = iframe.contentWindow;
+		win.postMessage(params, config.iframeURL);
+	};
 }
