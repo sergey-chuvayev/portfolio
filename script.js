32ca51a79603da9f7548ecbeea1fc17b93ca5116
Sergey <sergeypanic@gmail.com>
2015-10-15 19:11:09 +0300

    buttons

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ $(window).on("message", function(e) {
 
 	switch(e.originalEvent.data) {
 		case 'scrolledUnder':
-			console.log(e.originalEvent.data);
 			$('video').get(0).pause();
+			$('.button-play').show();
+			$('.button-pause').hide();
 			break;
 		case 'scrolledAbove':
-			console.log(e.originalEvent.data);
 			$('video').get(0).play();
+			$('.button-play').hide();
+			$('.button-pause').show();
 			break;
 		default:
 			initIFrame(e.originalEvent.data);
@@ -16,12 +18,24 @@ $(window).on("message", function(e) {
 });
 
 $(function() {
+	// add preloader on load
 	$('video').on('loadstart', function () {
-		$(this).attr("poster", "http://iphonewrd.com/img/loading.gif");
+		$(this).attr('poster', 'http://iphonewrd.com/img/loading.gif');
 	});
+
+	// remove preloader on play
 	$('video').on('canplay', function () {
-		$(this).removeAttr("poster");
+		$(this).removeAttr('poster');
 	});
+
+	// appear replay button and pause video
+    $('video').get(0).addEventListener('ended',function(){
+    	$('.button-replay').show();
+    	$(this).get(0).pause();
+		$('.button-pause').hide();
+		$('.button-mute').hide();
+		$('.button-unmute').hide();
+    },true);
 });
 
 function initIFrame(params) {
