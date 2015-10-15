606c615cb6f3c3d78a7740e1d1a728318760a0d8
Sergey <sergeypanic@gmail.com>
2015-10-15 13:09:00 +0300

    set background

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 	if (window.addEventListener) {
 		window.addEventListener("message", function(event) {
-			console.log(event.data);
-			initFrame(event.data);
+			initIFrame(event.data);
 		});
 	}
 
-function initFrame(params) {
+function initIFrame(params) {
 	$('video source').attr('src',params.video);
 	$('video').get(0).load();
+	$('.banner-container').css('background','url(' + params.bg + ')')
 }
