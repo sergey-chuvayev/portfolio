f96634cf172b7c2c107fe1bb265dda6415806cf5
Sergey <sergeypanic@gmail.com>
2015-10-15 15:08:51 +0300

    Revert "restructure dir for github.io"
    This reverts commit a72482d55236a787796b6605cd3f6980c6f55a04.

--- banner/script.js
+++
@@ -1,9 +0,0 @@
-$(window).on("message", function(e) {
-	initIFrame(e.originalEvent.data);
-});
-
-function initIFrame(params) {
-	$('video source').attr('src',params.video); // set video src
-	$('video').get(0).load();
-	$('.banner-container').css('background','url(' + params.bg + ')'); // set bg to banner
-}
--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-var config = {
-	iframeURL: 'http://localhost:4000/'
-}
-
-$(function() {
-
-	// set iframe url to iframe
-	$('iframe').attr('src', config.iframeURL);
-
-	// pass video and bg links to iframe
-	init({
-		video: 'http://lamcdn.net/specials.lookatme.ru/0000000001/banners/system/clients/top/guerlain/video2.mp4',
-		bg: 'http://content.adfox.ru/151007/adfox/503157/1402233_2.jpg'
-	});
+$(window).on("message", function(e) {
+	initIFrame(e.originalEvent.data);
 });
 
-function init(params) {
-	var iframe = $('iframe').get(0);
-	
-	// wait untill iframe loaded
-	iframe.onload = function() {
-		var win = iframe.contentWindow;
-		win.postMessage(params, config.iframeURL);
-	};
+function initIFrame(params) {
+	$('video source').attr('src',params.video); // set video src
+	$('video').get(0).load();
+	$('.banner-container').css('background','url(' + params.bg + ')'); // set bg to banner
 }
