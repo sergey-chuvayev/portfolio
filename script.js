b62e62f8e11858a21e2f8b961ea99c4900c4f272
Sergey <sergeypanic@gmail.com>
2015-10-17 15:59:58 +0300

    mute unmute button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var state = '';
 
-var Player = function($videoSelector, selectors) {
+var Player = function($videoSelector) {
 
-	this.selector = {
+	this.selectors = {
 		video: $videoSelector,
 		bReplay: $('.button-replay'),
 		bPause: $('.button-pause'),
@@ -11,51 +11,49 @@ var Player = function($videoSelector, selectors) {
 		bUnmute: $('.button-unmute')
 	}
 
-	console.log(this.selector);
-
-	var video = this.selector.video.get(0);
+	var video = this.selectors.video.get(0);
 	
 	this.pause = function() {
 		video.pause();
-		this.selector.bPlay.show();
-		this.selector.bPause.hide();
+		this.selectors.bPlay.show();
+		this.selectors.bPause.hide();
 		state = 'paused'
 	}
 	this.play = function() {
 		video.play();
-		this.selector.bPlay.hide();
-		this.selector.bPause.show();
+		this.selectors.bPlay.hide();
+		this.selectors.bPause.show();
 		state = 'playing'
 	}
 	this.replay = function() {
-		this.selector.bReplay.hide();
+		this.selectors.bReplay.hide();
 		video.currentTime = 0;
 		video.play();
 		state = 'replay'
 	}
 	this.hideAllControls = function() {
-		this.selector.bPlay.hide();
-		this.selector.bPause.hide();
-		this.selector.bReplay.hide();
-		this.selector.bMute.hide();
-		this.selector.bUnmute.hide();
+		this.selectors.bPlay.hide();
+		this.selectors.bPause.hide();
+		this.selectors.bReplay.hide();
+		this.selectors.bMute.hide();
+		this.selectors.bUnmute.hide();
 	}
 	this.mute = function() {
-		this.selector.bMute.show();
-		this.selector.bUnmute.hide();
-		$(video).attr('muted', true);
+		this.selectors.bMute.hide();
+		this.selectors.bUnmute.show();
+		$(video).prop('muted', true);
 		state = 'muted'
 	}
 	this.unmute = function() {
-		this.selector.bMute.hide();
-		this.selector.bUnmute.show();
-		$(video).attr('muted', false);
+		this.selectors.bMute.show();
+		this.selectors.bUnmute.hide();
+		$(video).prop('muted', false);
 		state = 'unmuted'
 	}
 }
 
+// create object on video
 var player = new Player($('video'));
-
 
 $(window).on("message", function(e) {
 
@@ -77,7 +75,13 @@ $(window).on("message", function(e) {
 	}
 
 	function notLockState() {
-		return state !== 'ended' && state !== 'manuallyPaued'
+		return state !== 'ended' && state !== 'manuallyPaued';
+	}
+
+	function initIFrame(params) {
+		player.selectors.video.parent().css('background','url(' + params.bg + ')'); // set bg to banner
+		player.selectors.video.children('source').attr('src',params.video); // set video src
+		player.selectors.video.get(0).load();
 	}
 
 });
@@ -85,13 +89,13 @@ $(window).on("message", function(e) {
 $(function() {
 
 	// add preloader on load
-	player.selector.video.on('loadstart', function () {
+	player.selectors.video.on('loadstart', function () {
 		$(this).attr('poster', 'http://iphonewrd.com/img/loading.gif');
 		player.hideAllControls();
 	});
 
 	// remove preloader on play
-	player.selector.video.on('canplay', function () {
+	player.selectors.video.on('canplay', function () {
 		console.log('canplay');
 		$(this).removeAttr('poster');
 		player.mute();
@@ -99,33 +103,27 @@ $(function() {
 	});
 
 	// appear replay button and pause video
-    player.selector.video.get(0).addEventListener('ended',function(){
+    player.selectors.video.get(0).addEventListener('ended',function(){
     	player.pause();
     	player.hideAllControls();
     	$('.button-replay').show();
     	state = 'ended'
     });
 
-    player.selector.bReplay.click(function(){
+    player.selectors.bReplay.click(function(){
     	player.replay();
     });
-    player.selector.bPause.click(function(){
+    player.selectors.bPause.click(function(){
     	player.pause();
     	state = 'manuallyPaued'
     });
-    player.selector.bPlay.click(function(){
+    player.selectors.bPlay.click(function(){
     	player.play();
     });
-    player.selector.bMute.click(function(){
+    player.selectors.bMute.click(function(){
     	player.mute();
     });
-    player.selector.bUnmute.click(function(){
+    player.selectors.bUnmute.click(function(){
     	player.unmute();
     });
 });
-
-function initIFrame(params) {
-	$('.banner-container').css('background','url(' + params.bg + ')'); // set bg to banner
-	$('video source').attr('src',params.video); // set video src
-	$('video').get(0).load();
-}
