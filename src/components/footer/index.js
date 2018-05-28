84d9708b7895a2033d01ac87058acaa6e96c28d9
Sergey <sergey@lobster.media>
2018-05-28 21:57:11 +0300

    added links

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,9 +9,9 @@ class Footer extends React.Component {
           You’re watching the portfolio of Sergey Chuvayev
         </div>
         <div className={styles['item']}>
-          <a href="facebook.com" target="_blanc">facebook</a>,&nbsp;
-          <a href="facebook.com" target="_blanc">soundcloud</a>,&nbsp;
-          <a href="facebook.com" target="_blanc">instagram</a>
+          <a href="https://www.facebook.com/sergeychuvayev" target="_blanc">facebook</a>,&nbsp;
+          <a href="https://soundcloud.com/sergeyaugust" target="_blanc">soundcloud</a>,&nbsp;
+          <a href="https://github.com/sergey-chuvayev/" target="_blanc">github</a>
         </div>
         <div className={styles['item']}>
           <a href="mailto: sergeypanic@gmail.com" target="_blanc">sergeypanic@gmail.com</a>
