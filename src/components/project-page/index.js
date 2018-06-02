b1bd148dc628d17f190fd99d6797c19cda8d1518
Sergey <sergey@lobster.media>
2018-06-02 18:47:02 +0300

    change images

--- src/components/project-page/index.js
+++ src/components/project-page/index.js
@@ -8,13 +8,15 @@ class ProjectPage extends React.Component {
     super(props);
 
     this.images = [
-      'https://i.pinimg.com/564x/4b/1f/0b/4b1f0b38f918213a1009d63fcbd049b8.jpg',
-      'https://i.pinimg.com/564x/a7/9b/1a/a79b1a3fb2ff57da98f343b8e9ec7151.jpg',
-      'https://i.pinimg.com/564x/a6/1b/a1/a61ba1313d81b1fc935c2f0798587e67.jpg',
-      'https://i.pinimg.com/564x/a6/48/ba/a648ba83f3b0ad99abe18b6c98ab14dd.jpg',
-      'https://i.pinimg.com/564x/f9/55/a3/f955a316c6c3f822459a8e9dae0759ae.jpg',
-      'https://i.pinimg.com/564x/d3/20/3a/d3203af31412b648722d77a82e71517f.jpg',
-      'https://i.pinimg.com/564x/3a/ba/95/3aba95640eb0f209d22753999d1d595a.jpg',
+      'https://storage.googleapis.com/sergeychuvayevportfolioimages/Screen%20Shot%202018-05-29%20at%2000.00.14.png',
+      'https://storage.googleapis.com/sergeychuvayevportfolioimages/Screen%20Shot%202018-05-29%20at%2000.04.30.png',
+      'https://storage.googleapis.com/sergeychuvayevportfolioimages/Screen%20Shot%202018-05-29%20at%2000.05.02.png',
+      'https://storage.googleapis.com/sergeychuvayevportfolioimages/Screen%20Shot%202018-05-29%20at%2000.05.11.png',
+      'https://storage.googleapis.com/sergeychuvayevportfolioimages/Screen%20Shot%202018-05-29%20at%2000.05.21.png',
+      'https://storage.googleapis.com/sergeychuvayevportfolioimages/Screen%20Shot%202018-05-29%20at%2000.05.37.png',
+      'https://storage.googleapis.com/sergeychuvayevportfolioimages/Screen%20Shot%202018-05-29%20at%2000.05.52.png',
+      'https://storage.googleapis.com/sergeychuvayevportfolioimages/Screen%20Shot%202018-05-29%20at%2000.05.57.png',
+      'https://storage.googleapis.com/sergeychuvayevportfolioimages/Screen%20Shot%202018-05-29%20at%2000.06.12.png',
     ];
 
     this.state = {
