34f3051fae3e6200befdb37ab5b88870d745cd88
Sergey <sergey@lobster.media>
2018-06-05 17:51:01 +0300

    см

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -13,7 +13,7 @@ class Footer extends React.Component {
           <a href="https://www.facebook.com/sergeychuvayev" target="_blanc">facebook</a>,&nbsp;
           <a href="https://soundcloud.com/sergeyaugust" target="_blanc">soundcloud</a>,&nbsp;
           <a href="https://github.com/sergey-chuvayev/" target="_blanc">github</a>,&nbsp;
-          <a href="https://www.slideshare.net/slideshow/embed_code/key/fYivGAxfd0vSRH" target="_blanc">CV</a>
+          <a href="https://storage.googleapis.com/sergeychuvayevportfolioimages/cv.pdf" target="_blanc">CV</a>
         </div>
         <div className={styles['item']}>
           <a href="mailto: sergeypanic@gmail.com" target="_blanc">sergeypanic@gmail.com</a>
