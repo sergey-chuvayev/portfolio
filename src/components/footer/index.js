00cee8b296e5790440e3baa003c3315ec87947cd
Sergey <sergey@lobster.media>
2018-06-26 13:55:45 +0100

    cv link

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -17,7 +17,7 @@ class Footer extends React.Component {
           <a href="https://www.facebook.com/sergeychuvayev" target="_blanc">facebook</a>,&nbsp;
           <a href="https://soundcloud.com/sergeyaugust" target="_blanc">soundcloud</a>,&nbsp;
           <a href="https://github.com/sergey-chuvayev/" target="_blanc">github</a>,&nbsp;
-          <a href="https://storage.googleapis.com/sergeychuvayevportfolioimages/cv.pdf" target="_blanc">CV</a>
+          <a href="https://storage.googleapis.com/sergeychuvayevportfolioimages/cv-sergey-chuvayev.pdf" target="_blanc">CV</a>
         </div>
         <div className={styles['item']}>
           <a href="mailto: sergeypanic@gmail.com" target="_blanc">sergeypanic@gmail.com</a>
