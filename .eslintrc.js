cb210859ad1ef3ee5e542960f52faf945b13fe75
Sergey <sergeychuvayev@gmail.com>
2018-12-12 20:35:37 +0300

    init project struct

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "no-useless-rename": ["error", {
+      "ignoreDestructuring": false,
+      "ignoreImport": false,
+      "ignoreExport": true
+    }]
+  }
+};
---
+++ src/components/root/root.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Root = () => <div />;
+
+export default Root;
